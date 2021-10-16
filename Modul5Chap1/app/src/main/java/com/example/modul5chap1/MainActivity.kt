cbaf3880361dcbe830d54398068f83f493130263
ArlPrima <81967164+ArlPrima@users.noreply.github.com>
2021-10-16 22:13:11 +0700

    PushTugas1
    Linear dan Relative Layout
    
    Co-Authored-By: McKael-forti <76393985+Mckael123@users.noreply.github.com>

---
+++ Modul5Chap1/app/src/main/java/com/example/modul5chap1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.modul5chap1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_main)
+    }
+}
